59d4496af7d8ea99bc3d03fd2aea82aa296bb06c
fgalbisr <felipe@manosextra.com>
2025-06-26 14:10:44 +0000

    Agregado page.tsx para ruta /sign-in

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en">
         <body>{children}</body>
       </html>
